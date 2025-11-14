6f588aac1980ca4cfcba12b0000a9fef0455ec8b
Graham Hukill <ghukill@gmail.com>
2025-11-14 09:43:13 -0500

    Update full_record strategy
    Why these changes are being introduced:
    
    With the json.dumps() approach, we had a lot of JSON characters and cruft in the
    final string used for embedding.
    
    How this addresses that need:
    
    Produces a <field>:<value><newline> repeating structure from the full record.
    
    Side effects of this change:
    * None
    
    Relevant ticket(s):
    * None

--- embeddings/strategies/full_record.py
+++ embeddings/strategies/full_record.py
@@ -9,5 +9,14 @@ class FullRecordStrategy(BaseStrategy):
     STRATEGY_NAME = "full_record"
 
     def extract_text(self, timdex_record: dict) -> str:
-        """Serialize the entire transformed_record as JSON."""
-        return json.dumps(timdex_record)
+        """Serialize the entire TIMDEX record.
+
+        The final string form is:
+            <field>: <value as JSON><newline>
+            <field>: <value as JSON><newline>
+            ...
+        """
+        final_string = ""
+        for k, v in timdex_record.items():
+            final_string += f"{k}: {json.dumps(v)}\n"
+        return final_string
